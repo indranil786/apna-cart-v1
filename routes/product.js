d6b62de1a88f9974caa8845a74f113ca6d39f132
Indranil Kar <karindranil786@hotmail.com>
2021-06-24 01:45:41 +0530

    fix : image fix

--- routes/product.js
+++ routes/product.js
@@ -17,7 +17,7 @@ const storage = multer.diskStorage({
     cb(null, path.join(__dirname, "/uploads/product"));
   },
   filename: (req, file, cb) => {
-    cb(null, file.fieldname + "-" + Date.now());
+    cb(null, file.fieldname + "_" + Date.now());
   },
 });
 
